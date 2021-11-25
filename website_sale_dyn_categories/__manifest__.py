28412edcfd85be9f78c1740db4a26930b9bffcd8
Saidi Oussama <saidi-ouss@outlook.com>
2021-11-25 12:59:20 +0100

    add image to category item

---
+++ website_sale_dyn_categories/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'Website Dynamic Categoreis',
+    'version': '1.0',
+    'summary': 'Website Dynamic Categories Show',
+    'category': 'website,sale',
+    'author': 'Azertics',
+    'maintainer': 'Azertics',
+    'website': 'www.azertics.com',
+  
+    'depends': [
+        'website','website_sale'
+    ],
+    
+    'data': [
+        'views/template.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
